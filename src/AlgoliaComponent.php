e16bf51084a25eba9496f9c5333964a01e6b826c
Juuso Leinonen <leinonen.juuso@gmail.com>
2017-01-14 13:32:19 +0200

    phpdoc for the getContext() to AlgoliaComponent

--- src/AlgoliaComponent.php
+++ src/AlgoliaComponent.php
@@ -43,6 +43,7 @@ use leinonen\Yii2Algolia\ActiveRecord\ActiveRecordFactory;
  * @method mixed request(Client $context, string $method, string $path, array $params, array $data, array $hostsArray, int $connectTimeout, int $readTimeout)
  * @method mixed doRequest(Client $context, string $method, string $path, array $params, array $data, array $hostsArray, int $connectTimeout, int $readTimeout)
  * @method \AlgoliaSearch\PlacesIndex initPlaces(string $appId, string $appKey, array $hostsArray = null, array $options = [])
+ * @method getContext()
  * @see Client
  * @see AlgoliaManager
  */
